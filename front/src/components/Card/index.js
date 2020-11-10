0420c20ddfaaeed15903a073ce9b70bc6bd266f2
Erick Major dos Santos <erickmajor@3227333.no-reply.drupal.org>
2020-11-10 11:50:37 -0300

    COMPONENTIZING: reestructuring the Card component.

--- front/src/components/Card/index.js
+++ front/src/components/Card/index.js
@@ -1,22 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
+import socket from '../../services/socket';
 
 import './styles.css';
 
-function Card (props) {
-  let className = 'Card';
-  if (props.active) {
-    className += ' active';
+export default class Card extends Component {
+  state = {
+    id: '',
+    value: '',
+    active: false
+  };
+
+  select = () => {
+    // Destructuring the state to get only our important data to us.
+    const { id } = this.state;
+    socket.emit('select-card', id);
+    this.setState({ active: true });
   }
 
-  const onClick = () => {
-    typeof props.onActivate == 'function' && props.onActivate(props.value);
+  deselect = () => {
+    // Destructuring the state to get only our important data to us.
+    const { id } = this.state;
+    socket.emit('deselect-card', id);
+    this.setState({ active: false });
   }
 
-  return (
-    <div className={className} onClick={onClick} >
-      <p className="Value">{props.value}</p>
-    </div>
-  );
+  reset = () => {
+    this.deselect();
+  }
+
+  componentDidMount() {
+    const { id, value } = this.props;
+
+    this.setState({ id, value, active: false });
+  }
+
+  render() {
+    // Destructuring the state to get only our important data to us.
+    const { value, active } = this.state;
+    const selected = active === true ? 'card selected' : 'card';
+
+    return (
+      <div className={selected} onClick={active === false ? this.select : this.deselect} >
+        <p className="card-value">{value}</p>
+      </div>
+    );
+  }
 }
-
-export default Card;
