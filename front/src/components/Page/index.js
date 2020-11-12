2bdb253f103dbc579c11d7926a6620736474955a
Erick Major dos Santos <erickmajor@3227333.no-reply.drupal.org>
2020-11-11 23:52:27 -0300

    PAGINATION: adjusting the Page component to mount the content area properly.

--- front/src/components/Page/index.js
+++ front/src/components/Page/index.js
@@ -5,8 +5,10 @@ import "./styles.css";
 export default class Page extends Component {
   render() {
     return (
-      <div className={`page ${this.props.id}-page`}>
+      <div className="page">
+        <div className={`${this.props.id}-page`}>
         {this.props.children}
+        </div>
       </div>
     );
   }
