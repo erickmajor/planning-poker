370f5d1654e0947826a13360ddd9d6d684ec3ea8
Erick Major dos Santos <erickmajor@3227333.no-reply.drupal.org>
2020-11-15 22:49:26 -0300

    PAGINATION: adding pages in the routes.

--- front/src/routes.js
+++ front/src/routes.js
@@ -2,12 +2,16 @@ import React from "react";
 
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import Main from "./pages/main";
+import MainPage from "./pages/main";
+import GamePage from "./pages/game";
+import DonePage from "./pages/done";
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
-      <Route exact path="/" component={Main} />
+      <Route exact path="/" component={MainPage} />
+      <Route path="/game" component={GamePage} />
+      <Route path="/done" component={DonePage} />
     </Switch>
   </BrowserRouter>
 );
